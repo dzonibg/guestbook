c6336675c0756dc32ce152113f55cb9918c6f661
Nikola <nidza.bg@gmail.com>
2019-11-15 19:54:30 +0100

    Fixed home page.

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+@extends("inc.layout")
+@section("page")
+    <div class="jumbotron">
+        <div class="container">
+            <h1>Welcome!</h1>
+            <p>Ths is a simple Laravel guestbook.</p>
+            <p>
+                <a class="btn btn-primary btn-lg">Learn more</a>
+            </p>
+        </div>
+    </div>
+@endsection
