3bf9a2980a538a0992f0730b0c5b2d2134b01775
Nikola <nidza.bg@gmail.com>
2019-12-06 01:10:31 +0100

    Added a Comment seeder & factory.

--- database/factories/CommentFactory.php
+++ database/factories/CommentFactory.php
@@ -7,6 +7,7 @@ use Faker\Generator as Faker;
 
 $factory->define(Comment::class, function (Faker $faker) {
     return [
-        //
+        'name' => $faker->name,
+        'text' => $faker->paragraph('1')
     ];
 });
